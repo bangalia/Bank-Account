b569f31b688a3d1157c1d1fdf555ce46fdd39a89
lotsofluck <anitabangali@yahoo.com>
2020-11-09 22:54:33 -0600

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -1,7 +1,25 @@
 class BankAccount:
     def __init__(self,full_name,account_number,routing_number,balance):
-        self.full_name
-        self.account_number
-        self.routing_number
-        self.balance
+        self.full_name = full_name
+        self.account_number = account_number
+        self.routing_number = routing_number
+        self.balance = balance
+
+def deposit(self,amount):
+    self.balance += amount
+
+def withdraw(self,amount):
+    overdraft= 10
+    if self.balance>=amount:
+        self.balance-=amount
+        print("You withdrew:$",amount)
+    else:
+        self.balance + overdraft = amount
+        print("Your balance is insufficient")
+    
+def getbalance():
+    return balance
+    print(f"Hello {full_name}! Your balance is {balance}")
+
+
         
